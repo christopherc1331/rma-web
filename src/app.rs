73f29ed879174c5aba4d507e5d19714e40000b31
Christopher Cruz <christopherc1331@gmail.com>
2024-09-15 23:19:33 -0500

    Got tailwind working

--- src/app.rs
+++ src/app.rs
@@ -1,7 +1,7 @@
 use leptos::*;
 use leptos_material::{
     components::{
-        button::ButtonType,
+        button::{Button, ButtonType},
         icon::Icon,
         iconbutton::{IconButton, IconButtonStyle},
         textfield::TextField,
@@ -18,7 +18,7 @@ pub fn App() -> impl IntoView {
 
     view! {
         <Stylesheet id="leptos" href="/pkg/rma-web.css" />
-
+        <Link href="https://fonts.googleapis.com/icon?family=Material+Icons"/>
         <Title text="RateMyArtist" />
         <UseMaterialWebComponents />
         <Router>
@@ -58,18 +58,55 @@ fn HomePage() -> impl IntoView {
                 <h1>Find Your Perfect Tattoo Artist</h1>
                 <p>Discover, rate, and review tattoo artists based on customer service and tattoo quality. Share your experience and help others find their ideal artist.</p>
                 <div class="search-container">
-                    <TextField attr:label="Search for artists or styles" value=search_val/>        <IconButton
-            style=IconButtonStyle::Filled
-            button_type=ButtonType::Submit
-            attr=vec![("aria-label", Attribute::String(Oco::Owned("Submit Button".to_string())))] // Convert array to Vec
-        >
-           "Submit"
-        </IconButton>
-                </div>
-        </div>
+                    <TextField attr:label="Search for artists or styles" value=search_val/>
+                <IconButton
+                    style=IconButtonStyle::Filled
+                    button_type=ButtonType::Submit
+                >
+            <span class="material-icons">favorite</span></IconButton>
+                <Button>Submit</Button>
+                    </div>
+            </div>
             <h1>"Welcome to Leptos!"</h1>
             <button on:click=on_click>"Click Me: " {count}</button>
             <h1>{"Hello from leptos-material!"}</h1>
+            <div class="wrapper px-2 w-full">
+            <form
+                action=""
+                class="max-w-sm bg-gray-100 px-3 py-5 rounded shadow-lg my-10 m-auto"
+            >
+                <div class="flex flex-col space-y-3">
+                <div
+                    class="flex items-center bg-white border border-gray-100 rounded px-2"
+                >
+                    <svg
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                        class="h-6 text-gray-500 m-0 mr-1"
+                    >
+                        <path
+                            d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"
+                        ></path>
+                        <path
+                            d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"
+                        ></path>
+                    </svg>
+                    <input
+                        type="text"
+                        placeholder="Email"
+                        class="w-full py-2 px-1 placeholder-indigo-400 outline-none placeholder-opacity-50"
+                        autocomplete="off"
+                    />
+                </div>
+                <button
+                    type="submit"
+                    class="text-white bg-indigo-500 px-4 py-2 rounded"
+                >
+                    Enviar
+                </button>
+                </div>
+            </form>
+        </div>
         </div>
     }
 }
