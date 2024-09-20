0e6d9d95f75680b49cbc0cbd240fe36fb0c8906b
Chris Cruz <christopherc1331@gmail.com>
2024-09-20 16:29:37 -0500

    Remove unused import

--- src/button.rs
+++ src/button.rs
@@ -1,4 +1,4 @@
-use leptos::{html::Button, *};
+use leptos::*;
 
 const BASE_STYLE: &str = "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50";
 
